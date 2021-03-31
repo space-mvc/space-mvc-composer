675a6994020aef8207d7ca7135f8863d719a7ee3
space-mvc <mrdaniellee2020@gmail.com>
2021-03-31 19:39:56 +0100

    added seeder class

---
+++ src/Library/Seed.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SpaceMvc\Framework\Library;
+
+use Phinx\Seed\AbstractSeed;
+
+/**
+ * Class Migration
+ * @package SpaceMvc\Framework\Library
+ */
+class Seed extends AbstractSeed
+{
+
+}
